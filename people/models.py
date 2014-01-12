e95989778d0189a105ee827c928d2f67fd297798
Furter <loraine.furter@gmail.com>
2014-01-12 11:49:01 +0100

    First Name not obligatory (for institutions contacts, etc)

--- people/models.py
+++ people/models.py
@@ -57,7 +57,7 @@ class FernandUser(AbstractBaseUser, PermissionsMixin):
     Username, password and email are required. Other fields are optional.
     """
 
-    first_name = models.CharField(_('first name'), max_length=30)
+    first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=30)
     email = models.EmailField(_('email address'), unique=True)
     alternate_email = models.EmailField(_('email address - alternate'), blank=True)
