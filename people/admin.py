7e2c497c943048c63fc6b27b2eaa44f6951c2254
Eric Schrijver <eric@ericschrijver.nl>
2015-04-21 13:43:40 +0200

    Fix missing field set variables on the FernandUserAdmin
    This set needs to be used to specify the fields that are used when
    creating a new user. Don’t know why they weren’t there, it is possible
    that the create new user form through the admin was not actually
    working. These fields could be tweaked to the Fernand site, now
    we just re-use the values from Liberationtech/django-libtech-emailuser.
    
    cf http://buildthis.com/customizing-djangos-default-user-model/
    cf https://github.com/Liberationtech/django-libtech-emailuser/blob/master/emailuser/admin.py

--- people/admin.py
+++ people/admin.py
@@ -27,6 +27,19 @@ class UserCategoryInline(admin.TabularInline):
 class FernandUserAdmin(admin.ModelAdmin):
     add_form_template = 'admin/auth/user/add_form.html'
     change_user_password_template = None
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2')}
+         ),
+    )
     form = FernandUserChangeForm
     add_form = FernandUserCreationForm
     change_password_form = AdminPasswordChangeForm
